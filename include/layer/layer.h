07cd125e5974399d085e978e51f7d24492594217
Dominik Aulinger <dominik@aulinger.tum.de>
2025-01-05 14:22:52 +0100

    add getComputeNode doc.

--- include/layer/layer.h
+++ include/layer/layer.h
@@ -6,5 +6,9 @@ public:
     virtual ~Layer() = default;
     std::shared_ptr<Layer> _previous;
     explicit Layer(const std::shared_ptr<Layer>& previous = nullptr);
+
+    /// Those should be renamed to outputs.
+    /// For conv layer those are the linearized image.
+    /// For fc layer those are the neuron output nodes.
     USE_RETURN virtual Vector<std::shared_ptr<ComputeNode>> getComputeNodes() const = 0;
 };
